8dce14d8654a17c94555ed41cbd1a452f5ed2a85
zalimpshigotizhev <zalimpshigotizhev@yandex.ru>
2024-01-24 20:46:35 +0300

    Готово на 70%.

--- timer/templatetags/zai_filters.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import unquote
-from django import template
-
-register = template.Library()
-
-@register.filter(name='urldecode')
-def urldecode(value):
-    return unquote(value)
